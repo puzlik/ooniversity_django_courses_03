6b61e662dcca0b935553e8ec076accbdf01ceece
puzlik <fursa.qa@gmail.com>
2015-11-26 20:16:48 +0200

    Changed in db and templates cause of adding new apps - courses and students

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from courses.models import Course
 
 def index(request):
-	return render(request, 'index.html')
+	courses = Course.objects.all()
+	return render(request, 'index.html', {
+		"courses": courses,
+		})
 
 def contact(request):
 	return render(request, 'contact.html')
