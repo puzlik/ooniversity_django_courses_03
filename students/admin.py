e1be3093e5ff8b277bc77c57f556965001cb1320
puzlik <fursa.qa@gmail.com>
2015-11-27 23:28:14 +0200

    Changes in search in the student-admin panel

--- students/admin.py
+++ students/admin.py
@@ -4,9 +4,9 @@ from students.models import Student
 
 class StudentAdmin(admin.ModelAdmin):
 	list_display = ('full_name', 'email', 'skype')
-	search_fields = ['surname', 'name']
+	search_fields = ('surname', 'email')
 	list_filter = ['courses']
-	filter_horizontal = ('courses', )
+	filter_horizontal = ['courses']
 
 	def full_name(self, obj):
 		return "%s %s" % (obj.name, obj.surname)
