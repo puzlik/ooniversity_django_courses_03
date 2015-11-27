a988899a0d4164af802a3962ac03b02c54e65054
puzlik <fursa.qa@gmail.com>
2015-11-27 22:42:58 +0200

    Changes in admin panel

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ('full_name', 'email', 'skype')
+	search_fields = ['surname', 'name']
+	list_filter = ['courses']
+	fieldsets = (
+		('Personal info', {
+			'fields': ('name', 'surname', 'date_of_birth')
+		}), 
+		('Contact info', {
+			'fields': ('email', 'phone', 'address', 'skype')
+		}),
+		(None, {
+			'fields': ('courses', ),
+		}), 
+	)
+
+	def full_name(self, obj):
+		return "%s %s" % (obj.surname, obj.name)
+
+admin.site.register(Student, StudentAdmin)
