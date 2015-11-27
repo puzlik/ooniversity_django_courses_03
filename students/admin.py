35dc2ef5e1c4fac7ec922c587afc63b5fe0f9d4b
puzlik <fursa.qa@gmail.com>
2015-11-27 22:58:05 +0200

    Added filter in courses-list for single student

--- students/admin.py
+++ students/admin.py
@@ -6,6 +6,11 @@ class StudentAdmin(admin.ModelAdmin):
 	list_display = ('full_name', 'email', 'skype')
 	search_fields = ['surname', 'name']
 	list_filter = ['courses']
+	filter_horizontal = ('courses', )
+
+	def full_name(self, obj):
+		return "%s %s" % (obj.name, obj.surname)
+
 	fieldsets = (
 		('Personal info', {
 			'fields': ('name', 'surname', 'date_of_birth')
@@ -17,8 +22,5 @@ class StudentAdmin(admin.ModelAdmin):
 			'fields': ('courses', ),
 		}), 
 	)
-
-	def full_name(self, obj):
-		return "%s %s" % (obj.surname, obj.name)
 
 admin.site.register(Student, StudentAdmin)
