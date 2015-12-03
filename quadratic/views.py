7a2ea070c92fbf0bc3d552583d5529094cecd0ba
puzlik <fursa.qa@gmail.com>
2015-12-03 02:39:57 +0200

    Added pictures, changed templates with base.html

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,68 +1,58 @@
 #-*-coding: utf-8-*-
 from django.shortcuts import render
+from quadratic.forms import QuadraticForm
+
+class Coefficient(object):
+	
+	def __init__(self, name, value):
+		self.name = name
+		self.value = value
+		self.value_int = None
+		self.error_message = None
+
+	def is_valid(self):
+		if not self.value:
+			self.error_message = "коэффициент не определен"
+			return False
+
+		try:
+			self.value_int = int(self.value)
+		except ValueError:
+			self.error_message = "коэффициент не целое число"
+			return False
+
+		if self.name == 'a' and self.value_int == 0:
+			self.error_message = "коэффициент при первом слагаемом уравнения не может быть равным нулю"
+			return False
+		return True
 
 def quadratic_results(request):
-	errors = list()
-	flag_digits = list()
-
-	def input_parameter(parameter_name = 'a'):
-		p = str(request.GET[parameter_name])
-		error_text = False
-		if p.replace('-', '').isdigit():
-			res = int(p)
-			flag_digit = True
-		elif len(p) == 0:
-			res = p
-			flag_digit = False
-			error_text = "коэффициент не определен"
+	context = {'error': False}
+	for name_value in ['a', 'b', 'c']:
+		coefficient = Coefficient(name_value, request.GET.get(name_value, ''))
+		if coefficient.is_valid():
+			context[name_value] = coefficient.value_int
 		else:
-			res = p
-			flag_digit = False
-			error_text = "коэффициент не целое число"
-		flag_digits.append(flag_digit)
-		errors.append(error_text)
-		return res
-
-	coef_a = input_parameter('a')
-	if str(request.GET['a']) == '0':
-		flag_digits[0] = False
-		errors[0] = "коэффициент при первом слагаемом уравнения не может быть равным нулю"
-	coef_b = input_parameter('b')
-	coef_c = input_parameter('c')
-
-	if False not in flag_digits:
-		a = int(coef_a)
-		b = int(coef_b)
-		c = int(coef_c)
+			context['error'] = True
+			context[name_value + '_error'] = coefficient.error_message
+			context[name_value] = coefficient.value
+	if not context['error']:
+		a = context['a']
+		b = context['b']
+		c = context['c']
 
 		d = b * b - 4 * a * c
 		if d < 0:
 			d_text = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-			x1 = False
-			x2 = False
 		elif d == 0:
 			x1 = - b / (2.0 * a)
-			x2 = False
 			d_text = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {0}".format(x1)
 		else:
 			x1 = (- b + d ** (1/2.0)) / (2 * a)
 			x2 = (- b - d ** (1/2.0)) / (2 * a)
 			d_text = "Квадратное уравнение имеет два действительных корня: x1 = {0}, x2 = {1}".format(x1, x2)
-		return render(request, 'results.html',{
-			'value_a': a,
-			'value_b': b,
-			'value_c': c,
-			'disc_eq': str(d),
-			'disc_text': d_text,
-			'first_root': x1,
-			'second_root': x2,
-			})
+		context['d_text'] = d_text
+		context['d'] = d
+		return render(request, 'results.html', context)
 	else:
-		return render(request, 'results.html',{
-			'value_a': coef_a,
-			'value_b': coef_b,
-			'value_c': coef_c,
-			'error_a': errors[0],
-			'error_b': errors[1],
-			'error_c': errors[2],
-			})
+		return render(request, 'results.html', context)
