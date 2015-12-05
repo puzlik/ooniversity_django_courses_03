64e7b0e24d1df4257d0b9077f735f50865b60277
puzlik <fursa.qa@gmail.com>
2015-12-06 00:04:46 +0200

    Try without redirect for edit kz 7_3

--- students/views.py
+++ students/views.py
@@ -54,7 +54,7 @@ def edit(request, pk):
 		form = StudentModelForm(request.POST, instance=student_by_id)
 		if form.is_valid:
 			form.save()
-			messages.success(request, "Info on the student has been successfully changed.")
+			messages.success(request, "Info on the student has been sucessfully changed.")
 	else:
 		form = StudentModelForm(instance=student_by_id)
 	context = {'form': form}
