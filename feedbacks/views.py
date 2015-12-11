f71d1a8cb64ed6d4b2cfc41bf53aa7d7d454bde9
puzlik <fursa.qa@gmail.com>
2015-12-11 12:48:44 +0200

    Update views.py

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -10,7 +10,7 @@ class FeedbackView(CreateView):
 	model = Feedback
 	template_name = 'feedback.html'
 	success_url = reverse_lazy('feedback')
-	context_object_name = 'form'
+	context_object_name = 'feedback'
 
 	def get_context_data(self, **kwargs):
 		context = super(FeedbackView, self).get_context_data(**kwargs)
@@ -18,9 +18,7 @@ class FeedbackView(CreateView):
 		return context
 
 	def form_valid(self, form):
-		
-		mail_admins(form.instance.subject, form.instance.message)
-
-		#email = EmailMessage(form.instance.subject, form.instance.message, form.instance.from_email, ['admin@pybursa.com'])
 		messages.success(self.request, u'Thank you for your feedback! We will keep in touch with you very soon!')
+		mail_admins(self.object.subject, self.object.message)
+		#email = EmailMessage(self.object.subject, self.object.message, self.object.from_email, ['admin@pybursa.com'])
 		return super(FeedbackView, self).form_valid(form)
