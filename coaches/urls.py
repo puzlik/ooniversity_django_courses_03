0cc5a022f311eb7989a5441f3b3a9c747d520ea6
puzlik <fursa.qa@gmail.com>
2015-12-11 21:02:20 +0200

    Forgot about coach.. not everything is done  kz 9 try 3

--- coaches/urls.py
+++ coaches/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from coaches import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>\d+)/$', views.CoachDetailView.as_view(), name='detail'),
 )
--- coaches/views.py
+++ coaches/views.py
@@ -2,19 +2,16 @@ from django.shortcuts import render
 from coaches.models import Coach
 from courses.models import Course
 from django.core.exceptions import ObjectDoesNotExist
+from django.views.generic.detail import DetailView
 
-def detail(request, pk):
-	try:
-		coach = Coach.objects.get(pk=pk)
-		coach.full_name = coach.user.get_full_name()
-		coach.email = coach.user.email
-		coach.courses_coach = Course.objects.filter(coach=coach)
-		coach.courses_assistant = Course.objects.filter(assistant=coach)
-		return render(request, 'coaches/detail.html', {
-	    	"coach": coach,
-	    	})
-	except ObjectDoesNotExist:
-		message = "Sorry, no coach with id = {0} exist yet.".format(pk) 
-		return render(request, 'coaches/detail.html', {
-		    "message": message,
-            })
+class CoachDetailView(DetailView):
+	model = Coach
+	template_name = 'coaches/detail.html'
+	context_object_name = 'coach'
+
+	def get_context_data(self, **kwargs):
+		coach = self.get_object()
+		context = super(CoachDetailView, self).get_context_data(**kwargs)
+		context['courses_coach'] = Course.objects.filter(coach=coach)
+		context['courses_assistant'] = Course.objects.filter(assistant=coach)
+		return context
