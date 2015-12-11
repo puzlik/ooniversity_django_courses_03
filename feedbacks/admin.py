1a8a909bedbb167275ea9c16c30a1385f3d5f894
puzlik <fursa.qa@gmail.com>
2015-12-11 12:40:19 +0200

    Update admin.py

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -3,9 +3,6 @@ from feedbacks.models import Feedback
 
 
 class FeedbackAdmin(admin.ModelAdmin):
-	list_display = ('from_email','time')
-
-	def time(self, obj):
-		return obj.create_date.time()
+	list_display = ('from_email','create_date')
 
 admin.site.register(Feedback, FeedbackAdmin)
