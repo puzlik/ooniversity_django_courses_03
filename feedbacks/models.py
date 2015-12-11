4720562dc25de5f8928375f3f13dda88b6607cfc
puzlik <fursa.qa@gmail.com>
2015-12-11 12:41:43 +0200

    Update models.py

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Feedback(models.Model):
 	name = models.CharField(max_length = 255)
 	subject = models.CharField(max_length = 255)
-	message = models.TextField()
+	message = models.TextField(blank=True)
 	from_email = models.EmailField()
-	create_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+	create_date = models.DateTimeField(auto_now_add=True)
 
 	def __unicode__(self):
 		return self.name
