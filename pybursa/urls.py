01fea23996b944df46ae63d628089ab6c2b1233d
puzlik <fursa.qa@gmail.com>
2015-12-11 12:39:16 +0200

    Update urls.py

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
     url(r'^student_list/$', views.student_list, name='student_list'),
     url(r'^student_detail/$', views.student_detail, name='student_detail'),
     url(r'^quadratic/', include('quadratic.urls'), name='results'),
-    url(r'^feedback/', include('feedbacks.urls'), name='feedback'),
+    url(r'^feedback/', views.FeedbackView.as_view(), name='feedback'),
 )
