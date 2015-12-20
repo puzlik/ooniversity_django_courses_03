9d99973fa9759bdf89509fb142e3afe3620f002a
puzlik <fursa.qa@gmail.com>
2015-12-20 10:27:11 +0200

    KZ 12_1

--- pybursa/views.py
+++ pybursa/views.py
@@ -25,7 +25,6 @@ def page_not_found(request):
     return response
 
 def internal_error(request):
-    response = render_to_response('500.html', {'message' : 'Sorry, internal server error occurred'},
-    							  context_instance=RequestContext(request))
+    response = render_to_response('500.html', { 'message' : 'Sorry, internal server error occurred' }, context_instance=RequestContext(request))
     response.status_code = 500
     return response
